602c1388757073ad068daeb757a18b1edd8532c1
piyumi_rameshka <rameshkafox@gmail.com>
2017-11-29 21:19:16 +0530

    adding doc comments

--- src/main/java/org/wso2/carbon/das/yarnapp/core/pojo/YarnContainer.java
+++ src/main/java/org/wso2/carbon/das/yarnapp/core/pojo/YarnContainer.java
@@ -1,9 +1,13 @@
 package org.wso2.carbon.das.yarnapp.core.pojo;
 
 import org.apache.hadoop.yarn.api.records.Container;
+import org.wso2.carbon.das.jobmanager.core.model.SiddhiAppHolder;
 
 import java.util.Map;
 
+/**
+ * This class holds allocated {@link Container} details along with the {@link SiddhiAppHolder}
+ */
 public class YarnContainer {
     private String parentAPPName;
     private Map<String,String> siddhiAppList;
