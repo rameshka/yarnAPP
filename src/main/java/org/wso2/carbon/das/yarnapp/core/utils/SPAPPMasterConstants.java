d5ae256443a580054932eaef2deb0746b46c8e84
piyumi_rameshka <rameshkafox@gmail.com>
2017-11-29 21:13:24 +0530

    APPMaster constants

--- src/main/java/org/wso2/carbon/das/yarnapp/core/utils/SPAPPMasterConstants.java
+++ src/main/java/org/wso2/carbon/das/yarnapp/core/utils/SPAPPMasterConstants.java
@@ -2,14 +2,18 @@ package org.wso2.carbon.das.yarnapp.core.utils;
 
 public class SPAPPMasterConstants {
 
-    public static final String SIDDHIAPP_LOCAL_FILE_PATH = "home/piyumi/";
     public static final String SP_LOCALIZED_NAME = "SPWorker.tar.gz";
-    public static final String SP_HDFS_NAME = "wso2sp-4.0.0-SNAPSHOT.tar.gz";
+    public static final String SP_HDFS_NAME = "wso2sp-4.0.0-beta2-SNAPSHOT.tar.gz";
     public static final Integer SP_PRIORITY_REQUIREMENT =0;
-    public static final Integer CONTAINER_MEMORY = 2048;
+    public static final Integer CONTAINER_MEMORY = 256;
     public static final Integer SP_VCORE=1; //the requrested vcore configuration is flexible as required vcore
     // configurations can be specified from yarn-site.xml
     public static final String SIDDHIAPP_HOLDER_HDFS_PATH ="siddhiappholderList.ser";
     public static final String SIDDHI_EXTENSION =".siddhi";
+    public static final String SPAPP_MASTER = "SPAPPMaster.jar";
+    public static final String SP_DEPLOYER_CLASS = "org.wso2.carbon.das.yarnapp.core.deployment.SPDeployer";
+    public static final String SP_UNIZIPPED_BUNDLE_NAME = "wso2sp-4.0.0-beta2-SNAPSHOT";
+    public static final String SERIALIZED_FILE_EXTENSION =".ser";
+
 
 }
