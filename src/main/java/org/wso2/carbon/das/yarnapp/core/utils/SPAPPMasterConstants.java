b8f9ef05ca9196f2b29d6906c10b16815a88c111
piyumi_rameshka <rameshkafox@gmail.com>
2017-11-29 22:06:24 +0530

    updating container memory value

--- src/main/java/org/wso2/carbon/das/yarnapp/core/utils/SPAPPMasterConstants.java
+++ src/main/java/org/wso2/carbon/das/yarnapp/core/utils/SPAPPMasterConstants.java
@@ -5,8 +5,8 @@ public class SPAPPMasterConstants {
     public static final String SP_LOCALIZED_NAME = "SPWorker.tar.gz";
     public static final String SP_HDFS_NAME = "wso2sp-4.0.0-beta2-SNAPSHOT.tar.gz";
     public static final Integer SP_PRIORITY_REQUIREMENT =0;
-    public static final Integer CONTAINER_MEMORY = 256;
-    public static final Integer SP_VCORE=1; //the requrested vcore configuration is flexible as required vcore
+    public static final Integer CONTAINER_MEMORY = 1024;
+    public static final Integer SP_VCORE = 1; //the requrested vcore configuration is flexible as required vcore
     // configurations can be specified from yarn-site.xml
     public static final String SIDDHIAPP_HOLDER_HDFS_PATH ="siddhiappholderList.ser";
     public static final String SIDDHI_EXTENSION =".siddhi";
